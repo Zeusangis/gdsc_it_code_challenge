c7c9034e6be44460541d3d270cd0e933a34d6e44
Unish Khadka <unishk152@gmail.com>
2024-10-16 00:02:03 -0500

    updated the database of users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,71 @@
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+from django.contrib.auth.models import Group, Permission
+import shortuuid
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError(_("User must have an email address"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        user = self.create_user(email, password, **kwargs)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    id = models.CharField(
+        _("ID"), primary_key=True, max_length=22, default=shortuuid.uuid, editable=False
+    )
+    email = models.EmailField(_("Email Address"), unique=True)
+    full_name = models.CharField(_("Full Name"), max_length=100)
+    profile_image = models.ImageField(
+        _("Profile Image"),
+        upload_to="profile/",
+        default="profile/default.png",
+        null=True,
+        blank=True,
+    )
+    is_active = models.BooleanField(_("Is Active"), default=True)
+    is_staff = models.BooleanField(_("Is Staff"), default=False)
+    date_joined = models.DateTimeField(_("Date Joined"), default=timezone.now)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',  # Unique related_name
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_set',  # Unique related_name
+        blank=True,
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name"]
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+        ordering = ["-date_joined"]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def login(request):
     return render(request, 'users/login.html')
 
 def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        if password == cpassword:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password)
+                user.save()
+                messages.info(request, 'User created')
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('register')
     return render(request, 'users/register.html')
