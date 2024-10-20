dae8f9ecc2b969d618c946934a586597a76f1cf5
Unish Khadka <unishk152@gmail.com>
2024-10-19 23:16:07 -0500

    updated the search

--- alumini/views.py
+++ alumini/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Alumini
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 
 @login_required(login_url="login")
@@ -8,12 +9,22 @@ def index(request):
     aluminis = Alumini.objects.values(
         "id", "full_name", "job_title", "name_of_business", "business_category"
     )
+
     if request.method == "POST":
-        name = request.POST.get("name")
-        business = request.POST.get("business_name")
-        aluminis = Alumini.objects.values(
-            "id", "full_name", "name_of_business", "business_category"
-        ).filter(full_name__icontains=name)
+        name = request.POST.get("name", "").strip()
+        business = request.POST.get("business_name", "").strip()
+
+        if name or business:
+            filters = Q()
+            if name:
+                filters &= Q(full_name__icontains=name)
+            if business:
+                filters &= Q(name_of_business__icontains=business)
+
+            aluminis = Alumini.objects.values(
+                "id", "full_name", "job_title", "name_of_business", "business_category"
+            ).filter(filters)
+
     context = {"aluminis": aluminis}
     return render(request, "alumini/index.html", context)
 
